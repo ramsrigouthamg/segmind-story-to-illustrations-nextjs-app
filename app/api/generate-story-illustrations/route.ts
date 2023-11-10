288339cf55eafd78dc52d61c6aee1e394d1e0c8e
Ramsri Goutham Golla <ramsrigouthamg@gmail.com>
2023-11-10 21:23:17 +0530

    Generate Story Illustration

---
+++ app/api/generate-story-illustrations/route.ts
@@ -0,0 +1,183 @@
+import { NextResponse, NextRequest } from 'next/server';
+export const maxDuration = 100; // This function can run for a maximum of 100 seconds
+export const dynamic = 'force-dynamic';
+
+
+
+const texttoimageURL = "https://api.segmind.com/v1/sdxl1.0-txt2img";
+const texttoimageAPIKEY = process.env.SEGMIND_API_KEY;
+
+const chat_gpt3_url = "https://api.openai.com/v1/chat/completions"
+const gpt3_headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+}
+
+console.log("gpt3_headers ", gpt3_headers)
+
+
+export const POST = async (req: NextRequest, res: Response) => {
+
+    const { text, style, imagecount } = await req.json();
+    console.log("text: ", text);
+    console.log("style: ", style);
+    console.log("imagecount: ", imagecount);
+
+
+    async function fetchIllustratorPrompts(prompt) {
+        // let retries = 0;
+        let currentAttempt = 0;
+        const maxRetryAttempts = 3;
+        let success = false;
+
+        while (currentAttempt < maxRetryAttempts && !success) {
+
+            try {
+                // Define the payload for the chat model
+                const messages = [
+                    {
+                        role: "system",
+                        content:
+                            "You are an expert author who can read children's stories and create short briefs for an illustrator, providing specific instructions, ideas, or guidelines for the illustrations you want them to create.",
+                    },
+                    { role: "user", content: prompt },
+                ];
+
+                const chatgptPayload = {
+                    model: "gpt-3.5-turbo-16k",
+                    messages: messages,
+                    temperature: 1.3,
+                    max_tokens: 2000,
+                    top_p: 1,
+                    stop: ["###"],
+                };
+
+
+
+
+                const response = await fetch(chat_gpt3_url, {
+                    headers: gpt3_headers,
+                    method: "POST",
+                    body: JSON.stringify(chatgptPayload),
+                    cache: 'no-cache'
+                });
+
+                // console.log("responseJson ",await response.json());
+
+                if (!response.ok) throw new Error("GPT-3 API fetch error");
+
+                const responseJson = await response.json();
+
+                console.log("responseJson ", responseJson)
+                console.log("message choices ",responseJson.choices[0].message.content.trim())
+
+                const output = JSON.parse(
+                    responseJson.choices[0].message.content.trim()
+                );
+
+                success = true;
+
+                return output;
+
+            } catch (err) {
+                currentAttempt++;
+                console.log(`Error on retry ${currentAttempt} parsing response or fetching data: `, err);
+                if (currentAttempt === maxRetryAttempts) {
+                    console.log("Max retries reached");
+                    return null;
+                }
+            }
+        }
+        // If max retries are reached without a successful response, return null
+        return null;
+    }
+
+
+    function generatePrompt(text, count) {
+        const promptPrefix = `${JSON.stringify(text)}
+  
+  ------------------------------------
+  
+  Generate ${count} short briefs as a list from the above story to give as input to an illustrator to generate relevant children's story illustrations.
+  
+  Strictly add no common prefix to briefs. Strictly generate each brief as a single sentence that contains all the necessary information.
+  
+  Strictly output your response in a JSON list format, adhering to the following sample structure:`;
+
+        const sampleOutput = { illustrations: Array(count).fill("...") };
+
+        const promptPostinstruction = `\nOutput:`;
+
+        return (
+            promptPrefix + JSON.stringify(sampleOutput) + promptPostinstruction
+        );
+    }
+
+    const prompt = generatePrompt(text, imagecount);
+
+
+    try {
+
+        const illustratorPrompts = await fetchIllustratorPrompts(prompt);
+        console.log("illustratorPrompts ", illustratorPrompts)
+
+        if (!illustratorPrompts || !illustratorPrompts.illustrations) {
+            throw new Error('Failed to fetch illustrator prompts');
+        }
+
+        const images = [];
+        for (const illustrationText of illustratorPrompts.illustrations) {
+            // Generate a random seed between 400000 and 500000
+            const randomSeed = Math.floor(Math.random() * (500000 - 400000 + 1)) + 400000;
+            const requestBody = {
+                prompt: illustrationText,
+                negative_prompt: "ugly, tiling, poorly drawn hands, poorly drawn feet, poorly drawn face, out of frame, extra limbs, disfigured, deformed, body out of frame, blurry, bad anatomy, blurred, watermark, grainy, signature, cut off, draft",
+                style: style,
+                samples: 1,
+                scheduler: "UniPC",
+                num_inference_steps: 25,
+                guidance_scale: "8",
+                strength: 0.2,
+                high_noise_fraction: 0.8,
+                // seed: "468685",
+                seed: randomSeed.toString(),
+                img_width: "1024",
+                img_height: "1024",
+                refiner: true,
+                base64: true,
+            };
+
+            console.log(requestBody)
+
+            const response = await fetch(texttoimageURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-api-key': texttoimageAPIKEY,
+                },
+                body: JSON.stringify(requestBody),
+            });
+
+            // console.log("response ",response);
+
+            if (!response.ok) {
+                throw new Error("Error with generating Illustrations");
+            }
+
+            // Assuming the response from the API is in image/jpeg format
+            const responseData = await response.json();
+            const base64Image = responseData.image;
+            images.push(base64Image);
+        }
+
+        return NextResponse.json({ images: images }, { status: 200 });
+
+
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ "message": `ERROR: Could not generate images for the text` }, { status: 500 });
+    }
+
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,341 @@
-import Image from 'next/image'
+"use client";
+
+import { useState,useEffect } from 'react';
+import ReactLoading from 'react-loading';
+// import Header from '@/components/Header'
+import Image from 'next/image';
+import DownloadIcon from '@mui/icons-material/GetApp';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/navigation'
+import 'swiper/css/thumbs'
+
+export const revalidate = 0;
 
 export default function Home() {
+  const [selectedOption, setSelectedOption] = useState('watercolor');
+  const [isLoading, setIsLoading] = useState(false);
+
+
+
+  const [textBoxValue, setTextBoxValue] = useState("Elon Musk has shown again he can influence the digital currency market with just his tweets. After saying that his electric vehicle-making company Tesla will not accept payments in Bitcoin because of environmental concerns, he tweeted that he was working with developers of Dogecoin to improve system transaction efficiency. \n\nFollowing the two distinct statements from him, the world's largest cryptocurrency hit a two-month low, while Dogecoin rallied by about 20 percent. The SpaceX CEO has in recent months often tweeted in support of Dogecoin, but rarely for Bitcoin.  In a recent tweet, Musk put out a statement from Tesla that it was concerned about the rapidly increasing use of fossil fuels for Bitcoin (price in India) mining and transaction, and hence was suspending vehicle purchases using the cryptocurrency. \n\nA day later he again tweeted saying, To be clear, I strongly believe in crypto, but it can't drive a massive increase in fossil fuel use, especially coal. It triggered a downward spiral for Bitcoin value but the cryptocurrency has stabilised since.  A number of Twitter users welcomed Musk's statement. One of them said it's time people started realising that Dogecoin is here to stay and another referred to Musk's previous assertion that crypto could become the world's future currency.");
+  const [outputValue, setOutputValue] = useState(null);
+
+
+  const [wordCount, setWordCount] = useState(0);
+  const [submitting, setIsSubmitting] = useState(false);
+
+  const [numImages, setNumImages] = useState(2);
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+
+
+
+  const handleNumImagesChange = (event) => {
+    setNumImages(parseInt(event.target.value, 10));
+  };
+
+  const handleDownload = (imageIndex) => {
+    // Fetch the image from the outputValue state using the index
+    const base64Image = outputValue[imageIndex];
+    // Create a link and set the URL using the base64Image data
+    const link = document.createElement("a");
+    link.href = `data:image/png;base64,${base64Image}`;
+    link.download = `illustration-${imageIndex}.png`;
+    // Append the link to the body, click it, and then remove it
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+
+  const DownloadButton = ({ onDownload, imageIndex }) => (
+    <button
+      onClick={() => onDownload(imageIndex)}
+      className="absolute top-2 right-2 bg-gray-200 bg-opacity-50 rounded-full p-2 hover:bg-gray-400 transition duration-300 ease-in-out"
+      aria-label="Download Image"
+    >
+      <DownloadIcon className="text-gray-600 hover:text-gray-800" /> {/* Use the MUI Icon */}
+    </button>
+  );
+
+  const calculateWordCount = (text) => {
+    const words = text.trim().split(/\s+/);
+    return words.length;
+  };
+
+  useEffect(() => {
+    const count = calculateWordCount(textBoxValue);
+    setWordCount(count);
+  }, []); // Empty dependency array to run the effect only once
+
+
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+
+  useEffect(() => {
+    if (outputValue) {
+      // setTotalCount(outputValue.length);
+    }
+  }, [outputValue]);
+
+  const handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    setTextBoxValue(inputValue);
+    const count = calculateWordCount(inputValue);
+    setWordCount(count);
+  };
+
+
+  const handleClick = async () => {
+
+    console.log("entered handleclick")
+
+
+    // Reset states
+    setOutputValue(null);
+    setIsSubmitting(false);
+
+    // setTotalCount(0);
+
+    setIsLoading(false);
+    try {
+
+
+
+      setIsLoading(true);
+
+      var requestOptions = {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          "text": textBoxValue,
+          "style": selectedOption,
+          "imagecount": numImages
+        }),
+        cache: 'no-cache'
+      };
+
+      console.log("requestOptions ", requestOptions)
+
+      const response = await fetch("/api/generate-story-illustrations", requestOptions);
+      // console.log('response ',response);
+
+      const newData = await response.json();
+      console.log('newData ', newData);
+
+      // Check if newData has a status property and if it's 429
+      if (response && response.status === 429) {
+        toast.error(newData.message);
+        return; // Exit from the function
+      }
+
+      setOutputValue(newData.images);
+
+
+
+    } catch (error) {
+      setOutputValue("An error occurred, please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="
+    bg-white-900 
+    rounded-lg 
+    h-full 
+    w-full 
+    overflow-hidden 
+    overflow-y-auto
+    p-4
+    ml-8
+    mr-8
+  ">
+
+
+<div className="flex flex-col items-center justify-center text-xl font-bold mb-4 text-black-500 md:text-2xl mr-4">
+  Generate Storybook Illustrations for children's stories using AI. 
+</div>
+
+<div className="flex flex-col items-center justify-center text-l font-bold mb-4 text-black-500 md:text-xl mr-4">
+  Use the dropdown selector to pick different illustration styles like Line Art, WaterColor, Comic Book etc
+</div>
+
+<div className="flex flex-col items-center justify-center text-md mb-4 text-black-500 md:text-lg mr-4">
+  Generate HD images at a 1024 x 1024 pixel resolution, ready for commercial use.
+</div>
+
+
+        <div className="flex w-full">
+
+          <div className="w-1/2 pr-2">
+
+
+            <div className={`text-md text-black-500`}>
+              Suggested text length: Upto 10,000 words. Supports English.
+            </div>
+
+            <div className={`text-sm font-semibold mb-2 ${wordCount >= 1 && wordCount <= 10000 ? 'text-green-700' : 'text-red-500'}`}>
+              <br />
+              Word Count: {wordCount}
+            </div>
+
+            <textarea
+              className="w-full h-[35vh] p-2 border rounded"
+              value={textBoxValue}
+              onChange={handleInputChange}
             />
-          </a>
+
+            <div className="w-full mt-2">
+              <label htmlFor="Style" className="block text-sm font-medium text-gray-700">Illustration Style</label>
+              <select
+                id="Style"
+                name="Style"
+                className="w-full p-2 mt-2 border rounded"
+                value={selectedOption}
+                onChange={handleChange}
+                style={{ maxWidth: '100%' }}
+              >
+                <option value="watercolor">Watercolor</option>
+                <option value="comic book">Comic Book</option>
+                <option value="line art">Line Art</option>
+                <option value="kawaii">Kawaii</option>
+
+              </select>
+            </div>
+
+            <div className="w-full mt-2">
+              <label htmlFor="Style" className="block text-sm font-medium text-gray-700">Image Count</label>
+              <select
+                id="numImages"
+                name="numImages"
+                className="w-full p-2 mt-2 border rounded"
+                value={numImages}
+                onChange={handleNumImagesChange}
+                style={{ maxWidth: '100%' }}
+              >
+                {[...Array(3).keys()].map((_, i) => (
+                  <option key={i} value={i + 1}>
+                    {i + 1}
+                  </option>
+                ))}
+
+              </select>
+            </div>
+
+            <button
+              className={`w-full mt-2 p-2 text-white rounded  'bg-green-500' }`}
+              onClick={ 
+                  handleClick
+              }
+              style={{
+                backgroundColor: '#1a8a2a',
+                cursor: 'pointer',
+                opacity: '1',
+              }}
+            >
+              Submit
+            </button>
+
+
+
+
+          </div>
+          <div className="w-1/2 pl-2 relative overflow-y-auto">
+            {isLoading && (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '50%',
+                  transform: 'translate(-50%, -50%)',
+                }}
+              >
+                <ReactLoading type="spin" color="#A9A9A9" />
+              </div>
+            )}
+
+
+
+            {outputValue && (
+              <div className="mt-8 mx-4">
+                {/* Thumbnail */}
+                <Swiper
+                  onSwiper={setThumbsSwiper}
+                  loop={true}
+                  spaceBetween={12}
+                  slidesPerView={4}
+                  freeMode={true}
+                  watchSlidesProgress={true}
+                  modules={[FreeMode, Navigation, Thumbs]}
+                  className='thumbs mt-3 h-32 w-full rounded-lg'
+                >
+                  {outputValue.map((base64Image, index) => (
+                    <SwiperSlide key={index}>
+                      <button className='flex h-full w-full items-center justify-center'>
+                        <Image
+                          src={`data:image/png;base64,${base64Image}`}
+                          alt={`Generated Illustration ${index}`}
+                          className='block h-full w-full object-cover'
+                          width={120} // specify the width
+                          height={120}
+
+                        />
+                      </button>
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+
+                <div className="mt-4" /> 
+
+                <Swiper
+                  loop={true}
+                  spaceBetween={10}
+                  navigation={true}
+                  thumbs={{
+                    swiper:
+                      thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+                  }}
+                  modules={[FreeMode, Navigation, Thumbs]}
+                  className='w-full rounded-lg'
+                >
+                  {outputValue.map((base64Image, index) => (
+                    <SwiperSlide key={index}>
+
+                      <div key={index} className="relative mb-4">
+                        <img
+                          src={`data:image/png;base64,${base64Image}`}
+                          alt={`Generated Illustration ${index}`}
+                          className="mb-4"
+                          // width={512}
+                          // height={512}
+                        />
+                        <DownloadButton onDownload={handleDownload} imageIndex={index} />
+                      </div>
+                    </SwiperSlide>
+                  ))}
+
+                </Swiper>
+
+
+                </div>
+
+
+
+            )}
+
+
+
+          </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
